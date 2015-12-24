bd1efd87c73a53e921d461e84413d9e9051e51dc
Galil <galilg@users.noreply.github.com>
2015-12-23 19:17:39 -0500

    Changed delete button, but still not functioning.  Now has ability to select user from user list

--- app/client/templates/user_list/user_list.js
+++ app/client/templates/user_list/user_list.js
@@ -2,17 +2,60 @@
 /* UserList: Event Handlers */
 /*****************************************************************************/
 Template.UserList.events({
-	'click .deleteThisGuy': function(e){
+	'click .deleteThisGuy': function(){
+
 		e.preventDefault();
 		console.log("its been hit");
 		console.log(Meteor.user().emails[0].address);
+	},
+
+	'click .userAccounts': function(){
+		Session.set('selectedUser', this._id);
+		var selectedUser = Session.get('selectedUser');
+		//console.log(selectedUser);
+	},
+
+	'click .theDeleteButton': function(e){
+		e.preventDefault();
+		console.log("Delete button pressed");
+		var userId = this._id;
+		var selectedUser = Session.get('selectedUser');
+		if(userId == selectedUser){
+			console.log("This is the guy getting deleted");
+			console.log(selectedUser);
+		}
 	}
+
+
+
 });
 
 /*****************************************************************************/
 /* UserList: Helpers */
 /*****************************************************************************/
 Template.UserList.helpers({
+	'userAccounts': function(){
+		return userDB.find();
+	},
+
+	'selectedClass': function(){
+		var userId = this._id;
+		var selectedUser = Session.get('selectedUser');
+		if(userId == selectedUser){
+			return "selected";
+		}
+	}, 
+
+	'deleteThisGuy': function(){
+		var userId = this._id;
+		var selectedUser = Session.get('selectedUser');
+		if(userId == selectedUser){
+			console.log("This is the guy getting deleted");
+			console.log(selectedUser);
+
+		}
+	},
+
 	beforeRemove: function() {
 		//return function (collection, id) {
 			//var doc = collection.findOne(id);
