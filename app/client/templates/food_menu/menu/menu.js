db2ea994c30140a9a811b1f59899fe3a94e33690
Galil <galilg@users.noreply.github.com>
2016-01-14 10:27:49 -0500

    Menus page now has functioning tabs switching from lunch, brunch, dinner, and bev menus.

--- app/client/templates/food_menu/menu/menu.js
+++ app/client/templates/food_menu/menu/menu.js
@@ -2,21 +2,52 @@
 /* Menu: Event Handlers */
 /*****************************************************************************/
 Template.Menu.events({
+	'click #restMenu': function(e){
+		e.preventDefault();
+		$('a[href="#0"]').tab('show');
+	},
+
+	'click #lunch': function(e){
+		e.preventDefault();
+		//console.log("Felt the click");
+		$('a[href="#1"]').tab('show');
+
+	},
+
+	'click #brunch': function(e){
+		e.preventDefault();
+		$('a[href="#2"]').tab('show');
+
+	},
+
+	'click #dinner': function(e){
+		e.preventDefault();
+		$('a[href="#3"]').tab('show');
+
+	},
+
+	'click #bev': function(e){
+		e.preventDefault();
+		$('a[href="#4"]').tab('show');
+	}
 });
 
 /*****************************************************************************/
 /* Menu: Helpers */
 /*****************************************************************************/
 Template.Menu.helpers({
+	
 });
 
 /*****************************************************************************/
 /* Menu: Lifecycle Hooks */
 /*****************************************************************************/
 Template.Menu.onCreated(function () {
+
 });
 
 Template.Menu.onRendered(function () {
+
 });
 
 Template.Menu.onDestroyed(function () {
