16ef2f7ffe522ec1666b3c15c03fdf890d498526
Galil <galilg@users.noreply.github.com>
2016-01-27 02:22:35 -0500

    View event now only displays tables created from that event, and sorts them by table name -- however, since I left table name as a string rather than a integer, it is sorted accoriding to the place of the digit, as if it were an alpha character.  That is, if the digits have different string lengths, 10001 would come after 100, but before 200, because it compares first character first, then next characters.  Not the best, but I want to allow tables to have names.  Perhaps I will change this.

--- app/client/templates/events/view_event/view_event.js
+++ app/client/templates/events/view_event/view_event.js
@@ -13,7 +13,21 @@ Template.ViewEvent.helpers({
 		var selectedEvent = Session.get('currentEvent');
 		var theEvent = Events.findOne({_id: selectedEvent});
 		return theEvent;
-	}
+	},
+
+	tables: function() {
+		//var theUser = Meteor.users.find({_id: this.userId});
+		//if (!Meteor.user().profile.businessName){  					// If it is the root user, they don't have a profile.businessName
+		//	var businessType = Meteor.user().emails[0].address; 	// So in that case the businessType takes their email
+		//}															// Which serves as the businessType for all children of the root user
+		//else{
+		//	var businessType = Meteor.user().profile.businessName;	// Otherwise, businessType gets assigned the profile.businessName of the regular child user
+		//}
+		var currentEvent = Session.get('currentEvent');
+		return Tables.find({createdForEvent: currentEvent}, {sort: {tableName: 1}});
+
+	},
+
 });
 
 /*****************************************************************************/
--- app/lib/controllers/events_controller.js
+++ app/lib/controllers/events_controller.js
@@ -7,6 +7,7 @@ EventsController = RouteController.extend({
   
   subscriptions: function() {
     this.subscribe('events');
+    this.subscribe('tables');
   },
   
   // Subscriptions or other things we want to "wait" on. This also
