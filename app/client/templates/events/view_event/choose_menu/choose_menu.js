02224ddbc9bb58cd623f58f73134182d16143d8f
Galil <galilg@users.noreply.github.com>
2016-02-12 02:18:54 -0500

    Added select buttons to the food choice menu, but not functional yet... do I need a submit button to create yet another collection? Or should the select button just toggle an attribute of the food item 'selected'?

--- app/client/templates/events/view_event/choose_menu/choose_menu.js
+++ app/client/templates/events/view_event/choose_menu/choose_menu.js
@@ -9,13 +9,9 @@ Template.ChooseMenu.events({
 /*****************************************************************************/
 Template.ChooseMenu.helpers({
 	aMenu: function() {
-		console.log("GOT IN HERE");
 		var businessType = Meteor.user().profile.businessName;
 		var selectedEvent = Session.get('currentEvent');
-		console.log(selectedEvent);
 		var eventType = Events.findOne({_id:selectedEvent}).eventType;
-		console.log(eventType);
-		console.log(Menus.find({}));
 		return Menus.find({createdFromAccount: businessType, category:eventType}, {sort: {course:1, itemName:1}});
 	}
 });
